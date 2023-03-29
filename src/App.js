5213cf2303751377eb3ccea260c8c5ba28c00b49
Stas <singol.draft@gmail.com>
2023-03-29 14:08:52 +0500

    tg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { useEffect } from 'react';
 import './App.css';
 
+const tg = window.Telegram.WebApp
+
 function App() {
 
   useEffect(() => {
     tg.ready()
   }, [])
-
 
   return (
     <div className="App">
