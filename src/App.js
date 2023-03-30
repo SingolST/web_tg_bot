2b7f02a8f4556ad8b5f343fdbb596b818d657439
Stas <singol.draft@gmail.com>
2023-03-30 10:28:21 +0500

    burger2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import { useEffect } from 'react';
 import './App.css';
-import { useTelegram } from './hooks/useTelegram';
 
 
 function App() {
-  const {tg} = useTelegram();
-
-  useEffect(() => {
-    tg.ready()
-  }, [])
-
   return (
     <div className="App">
       <p>МЕНЮ</p>
